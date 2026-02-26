7de6a20d91f65fe98d3c7d8cab814593c0098157
wanderson360 <wandersondevpy@gmail.com>
2026-02-26 13:05:13 -0300

    Add task mod5

---
+++ mod5/FirstClass/src/br/ebac/wanderson/FirstClass.java
@@ -0,0 +1,9 @@
+package br.ebac.wanderson;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Wanderson.");
+	}
+
+}
---
+++ mod5/FirstClass/src/br/ebac/wanderson/package-info.java
@@ -0,0 +1,1 @@
+package br.ebac.wanderson;
